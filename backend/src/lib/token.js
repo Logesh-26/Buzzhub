00c2f1072fd807cc489b65ad62d0d35db2df74da
Vkhari88 <vkhari040@gmail.com>
2026-04-17 18:40:24 +0530

    revert changes

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -2,13 +2,11 @@ import jwt from "jsonwebtoken";
 
 export const tokenGeneration = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.secretkey, { expiresIn: "14d" });
-    const isProduction = process.env.NODE_ENV === "production";
-
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure: isProduction,
-        sameSite: isProduction ? "none" : "lax",
+        secure: true,
+        sameSite: "none",
     });
 };
 
