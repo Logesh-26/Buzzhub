8b290f3666b0c2b6a1009fc88fb2983aae6c0448
Vkhari88 <vkhari040@gmail.com>
2026-04-17 18:55:56 +0530

    changes done.

--- backend/src/middelwares/authMiddleware.js
+++ backend/src/middelwares/authMiddleware.js
@@ -5,15 +5,16 @@ export const checkAuth = async (req, res, next) => {
     try {
         const token = req.cookies.jwt
         if (!token) {
-            res.status(400).json({ message: "token is required." })
+            return res.status(400).json({ message: "token is required." })
         }
-        const decode = jwt.verify(token, process.env.secretkey)
+        const decode = jwt.verify(token, process.env.secretkey);
+        
         if (!decode) {
-            res.status(401).json({message:"Unauthorized-Invaild token"})
+            return res.status(401).json({message:"Unauthorized-Invaild token"})
         }
-        const user = await User.findById(decode.userId).select("-passoword")
+        const user = await User.findById(decode.userId).select("-password")
         if (!user) {
-            res.status(404).json({message:"user not found"})
+            return res.status(404).json({message:"user not found"})
         }
         req.user = user
         next()
